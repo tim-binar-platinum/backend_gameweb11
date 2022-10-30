f508dce98183880b71dab6709659a7e4dce7d77d
emillio <emilliokanz@gmail.com>
2022-10-30 18:11:36 +0700

    features:  - CRUD for user datas  - jwt token generate after login  - middleware auth using jwt bearer token

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
 const Express = require('express');
 const router = require('./router');
 const models = require('./models');
-const session = require('express-session');
 const app = Express();
 
 app.use(Express.json())
 app.use(Express.urlencoded({ extended: false }));
-
-// using session for middleware
-app.use(session({
-  secret: '199719',
-  resave: false,
-  saveUninitialized: false
-}))
-
-app.use(Express.static('static'))
-
-app.use('/router', Express.static('static'))
 app.use(router)
 
 models.sequelize.authenticate().then(() => {
